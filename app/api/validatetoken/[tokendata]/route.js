c6216f45a1de5ec6d68913973487e510dfda19ea
Carlos Francisco Estrada Garcidueñas <cestradagar@guanajuato.gob.mx>
2024-09-20 22:17:01 -0600

    Ruta de validaciòn

---
+++ app/api/validatetoken/[tokendata]/route.js
@@ -0,0 +1,14 @@
+import { verifyToken } from "@/lib/validateToken";
+
+export const revalidate = 0;
+export async function GET(request, { params }) {
+  try {
+    const { tokendata } = params;
+
+    const token = await verifyToken(tokendata);
+
+    return Response.json(token);
+  } catch (e) {
+    return Response.json({ error: e.message }, { status: 500 });
+  }
+}
