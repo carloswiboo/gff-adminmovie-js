638d0adb9815b2ed665937146e85e2348f66aa8d
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2024-09-20 22:22:02 -0600

    Poniendo ejemplo de submission

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import * as Yup from "yup";
 export default function Component() {
   const formik = useFormik({
     initialValues: {
-      submission_id: "",
-      email: "",
+      submission_id: "23982164",
+      email: "rebecca.metcalf01@gmail.com",
     },
     validationSchema: Yup.object({
       submission_id: Yup.string().required("Submission ID is required"),
