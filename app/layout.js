7505c2e2ea4e454d1eacc18634eb98700e628444
dgitsesea <dgit.sesea@guanajuato.gob.mx>
2025-09-23 17:12:46 -0600

    Agregando Flowbite

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-
         <Toaster />
       </body>
     </html>
